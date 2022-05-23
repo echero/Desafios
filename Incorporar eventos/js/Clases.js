4e33b2589351e78d18523be8a9d6bed1a5513265
echero <chero2005@hotmail.com>
2022-05-23 17:04:44 -0300

    Desafio Eventos

---
+++ Incorporar eventos/js/Clases.js
@@ -0,0 +1,10 @@
+export class Auto {
+    constructor(marca, modelo, año, cantPuertas, precio, imagen){
+        this.marca = marca
+        this.modelo = modelo
+        this.año = año
+        this.cantPuertas = cantPuertas
+        this.precio = precio
+        this.imagen = imagen
+    }
+}
